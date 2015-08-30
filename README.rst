6a0d28a94f395d9b83f743ebc2a766d98b21b62d
Tim Savannah <kata198@gmail.com>
2015-08-30 18:47:32 -0400

    Initial Checkin

---
+++ README.rst
@@ -0,0 +1,57 @@
+# remote\_copy\_and\_execute
+
+remote\_copy\_and\_execute is a tool used to copy a script in batch to a set list of hosts, execute N at a time, and print the results.
+
+It uses the SSH protocol (ssh and scp utilities) to perform the acts. This is useful for a multiude of purposes, 
+
+from running custom audit scripts to deployment scripts, activation and really any batch task.
+
+
+Example Usage:
+--------------
+
+
+	remote_copy_and_execute --rcae-at-a-time=3 --rcae-as-user=www --rcae-skip-bad-hosts ./myScriptName -- host1 host2 host3 host4 host5 host6 host7
+
+
+The above command will copy and execute execute as "www" the script "myScriptName" on 3 hosts at a time, until all given hosts are completed or failed, and print results on completion.
+
+
+	remote_copy_and_execute --rcae-batch --rcae-as-user=myuser /home/myuser/scripts/audit -- host1 host2 host3 host4
+
+The above command will copy and execute execute "/home/myuser/scripts/audit" on all of the given hosts, as "myuser", using common batch options.
+
+
+All Options:
+------------
+
+
+	Usage: remote_copy_and_execute [program] [args] (--) [hostname1] [hostnameN]
+	Copies a script and executes on multiple hosts simultaneously. Use "--" after the args and before the list of host names.
+	Script must be executable by the running user
+
+	remote_copy_and_execute arguments:
+
+		--rcae-timeout=#seconds         to use a timeout.
+		--rcae-omit-empty               Omit printing empty results
+		--rcae-at-a-time=#              Split application into given # chunks
+		--rcae-hide-date                Do not show runtime date
+		--rcae-skip-bad-hosts           Skip bad hosts. Default is to terminate.
+		--rcae-quiet                    Omit all output except that from the script. Implies hide-date
+		--rcae-as-user=username         Perform copy and execute as given user. Default is root.
+		--rcae-print-on-host-complete   Print right after each host completes execution. Default is to print at end of each set. Assumes rcae-hide-date.
+
+		--rcae-batch                    Sets defaults [listed below]. Sane defaults for batch
+				                          executions. This directive is evaluated first, so you can override
+				                          the ones that take a paramater.
+
+					                        timeout=2
+					                        omit-empty
+					                        at-a-time=15
+					                        hide-date
+					                        skip-bad-hosts
+
+
+		--rcae-help                     Show this message
+
+
---
+++ setup.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+
+if __name__ == '__main__':
+
+    try:
+        with open('README.rst', 'r') as f:
+            long_description = f.read()
+    except:
+        long_description = ''
+
+    setup(name='remote_copy_and_execute',
+            version='1.1',
+            scripts=['remote_copy_and_execute'],
+            author='Tim Savannah',
+            author_email='kata198@gmail.com',
+            maintainer='Tim Savannah',
+            url='https://github.com/kata198/remote_copy_and_execute',
+            maintainer_email='kata198@gmail.com',
+            description='Tool to use SSH protocol to copy and execute arbitrary scripts/commands on a list of machines in parallel',
+            long_description=long_description,
+            license='GPLv3',
+            keywords=['ssh', 'scp', 'remote', 'copy', 'execute', 'shell', 'rcae', 'script', 'host'],
+            classifiers=['Development Status :: 5 - Production/Stable',
+                         'Programming Language :: Python',
+                         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                         'Programming Language :: Python :: 2',
+                          'Programming Language :: Python :: 2',
+                          'Programming Language :: Python :: 2.7',
+                          'Programming Language :: Python :: 3',
+                          'Programming Language :: Python :: 3.3',
+                          'Programming Language :: Python :: 3.4',
+                          'Topic :: Internet :: WWW/HTTP',
+                          'Environment :: Console',
+                          'Intended Audience :: System Administrators',
+                          'Intended Audience :: Developers',
+                          'Topic :: System :: Distributed Computing',
+                          'Topic :: System :: Software Distribution',
+                          'Topic :: Utilities',
+            ]
+    )
+
+
+
+exampleProgram = """
+import AdvancedHTMLParser
+
+parser = AdvancedHTMLParser.AdvancedHTMLParser()
+
+parser.parseStr('''
+<html>
+ <head>
+  <title>HEllo</title>
+ </head>
+ <body>
+   <div id="container1" class="abc">
+     <div name="items">
+      <span name="price">1.96</span>
+     <span name="itemName">Sponges</span>
+   </div>
+   <div name="items">
+     <span name="price">3.55</span>
+     <span name="itemName">Turtles</span>
+   </div>
+   <div name="items">
+     <span name="price" class="something" >6.55</span>
+     <img src="/images/cheddar.png" style="width: 64px; height: 64px;" />
+     <span name="itemName">Cheese</span>
+   </div>
+ </div>
+ <div id="images">
+   <img src="/abc.gif" name="image" />
+   <img src="/abc2.gif" name="image" />
+  </div>
+  <div id="saleSection" style="background-color: blue">
+    <div name="items">
+      <span name="itemName">Pudding Cups</span>
+      <span name="price">1.60</span>
+    </div>
+    <hr />
+    <div name="items" class="limited-supplies" >
+      <span name="itemName">Gold Brick</span>
+      <span name="price">214.55</span>
+      <b style="margin-left: 10px">LIMITED QUANTITIES: <span id="item_5123523_remain">130</span></b>
+    </div>
+  </body>
+</html>
+ ''')
+
+print ( "Items less than $4.00: ")
+print ( "-----------------------\n")
+items = parser.getElementsByName('items')
+
+parser2 = AdvancedHTMLParser.AdvancedHTMLParser()
+parser2.parseStr('<div name="items"> <span name="itemName">Coop</span><span name="price">1.44</span></div>')
+
+items[0].parentNode.appendChild(parser2.getRoot())
+items = parser.getElementsByName('items')
+
+for item in items:
+    priceEm = item.getElementsByName('price')[0]
+
+    priceValue = round(float(priceEm.innerHTML.strip()), 2)
+    if priceValue < 4.00:
+        name = priceEm.getPeersByName('itemName')[0].innerHTML.strip()
+
+        print ( "%s - $%.2f" %(name, priceValue) )
+
+
+# OUTPUT:
+# Items less than $4.00: 
+# -----------------------
+# 
+# Sponges - $1.96
+# Turtles - $3.55
+# Pudding Cups - $1.60
+
+"""
+
+#vim: set ts=4 sw=4 expandtab
